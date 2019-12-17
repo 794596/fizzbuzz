898d6c2fd16c71af32985a0a4854672fedffcbb8
Anthony Tithecott-John <794596@swansa.ac.uk>
2019-12-17 21:07:37 +0000

    Tests Changed to tables and fizz_buzz one line

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
+"""
+print a string with the numbers between 1 and the maximum separated by ", ".
+If a number is a multiple of 3, it is replaced by fizz.
+If a number is a multiple of 5, it is replaced by buzz.
+If a number is a multiple of both 3 and 5, it is replaced by FizzBuzz
+Otherwise the number is returned.
+"""
 FIZZ_CONSTANT = 3
 BUZZ_CONSTANT = 5
 
 
 def fizz_buzz(number):
     """
-    Returns fizzbuzz if number is a multiple of 5 and 3,
-    returns fizz if number is a multiple of 5,
-    returns buzz if number is a multiple of 3,
+    returns FizzBuzz if number is a multiple of 5 and 3,
+    returns Fizz if number is a multiple of 5,
+    returns Buzz if number is a multiple of 3,
     returns the number of it is none of the above.
 
+    parameters:
+        number (int): number to be determined what is returned
+
+    return:
+        str: value related to the number in fizzbuzz
+
     """
-    if (number % FIZZ_CONSTANT == 0) & (number % BUZZ_CONSTANT == 0):
-        return "FizzBuzz"
-    elif number % FIZZ_CONSTANT == 0:
-        return "Fizz"
-    elif number % BUZZ_CONSTANT == 0:
-        return "Buzz"
-    else:
-        return number
+    try:
+        return "Fizz"*(number % 3 == 0)+"Buzz"*(number % 5 == 0) or str(number)
+    except TypeError:
+        return None
 
 
 def string_fizz_buzz(maximum):
     """
-    Iterates through the range 0 to maximum calls the "fizz_buzz" function adds to string.
-    prints out the string.
+    iterates through the range 0 to maximum calls the "fizz_buzz" function adds to string.
+    Prints out the string.
+
+    Parameter:
+        maximum (int): number for loop iterates to.
+
+    Return:
+        none
+
     """
     string = []
     for integer in range(0, (maximum+1)):
--- mytests.py
+++ mytests.py
@@ -3,11 +3,62 @@ import main
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_numbers(self):
-        self.assertEqual(main.fizz_buzz(1), 1)
+    def test_number(self):
+        test_cases = [
+            {
+                "name": "Simple number test 1",
+                "input": 1,
+                "expected": "1"
+            },
+            {
+                "name": "Simple number test 2",
+                "input": 2,
+                "expected": "2"
+            },
+            {
+                "name": "Simple number test 3",
+                "input": 997,
+                "expected": "997"
+            },
+            {
+                "name": "Test imput of none",
+                "input": None,
+                "expected": None
+            },
+            {
+                "name": "test input of string",
+                "input": "word",
+                "expected": None
+            }
+        ]
+        for test_case in test_cases:
+            assert test_case["expected"] == main.fizz_buzz(test_case["input"]), test_case["name"]
 
-    def test_multiple_of_three(self):
-        self.assertEqual(main.fizz_buzz(3), "Fizz")
+    def test_multiple_of_3(self):
+        test_cases = [
+            {
+                "name": "test multiple of three 1",
+                "input": 3,
+                "expected": "Fizz"
+            },
+            {
+                "name": "test multiple of three 2",
+                "input": 6,
+                "expected": "Fizz"
+            },
+            {
+                "name": "test multiple of three 3",
+                "input": 999,
+                "expected": "Fizz"
+            },
+            {
+                "name": "test multiple of three with string",
+                "input": "999",
+                "expected": None
+            }
+        ]
+        for test_case in test_cases:
+            assert test_case["expected"] == main.fizz_buzz(test_case["input"]), test_case["name"]
 
     def test_multiple_of_five(self):
         self.assertEqual(main.fizz_buzz(5), "Buzz")
